12b840b06884656046c56c276695859a6936c035
Suraj Rajbhar <suraj.rajbhar.tech@gmail.com>
2025-10-08 21:30:15 +0530

    finished env

--- frontend/src/store/TodoStore.js
+++ frontend/src/store/TodoStore.js
@@ -2,7 +2,8 @@ import { create } from 'zustand'
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
 
-const api = "http://localhost:5000/api"
+const api = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
 const useTodoStore = create((set, get) => ({
     todos: [],
     isCreating: false,
