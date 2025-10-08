2c86ef014f1f7ea4eeedae0b72420735f83ebc2d
Suraj Rajbhar <suraj.rajbhar.tech@gmail.com>
2025-10-08 22:37:26 +0530

    backend

--- frontend/src/store/TodoStore.js
+++ frontend/src/store/TodoStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
 
-const api = import.meta.env.VITE_API_URL || "http://localhost:5000/api" || "https://bizdateup-todos.onrender.com/api";
+const api = "https://my-backend.onrender.com/api" || "http://localhost:5000/api";
 
 const useTodoStore = create((set, get) => ({
     todos: [],
